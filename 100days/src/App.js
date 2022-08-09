64052ae5128f455eae2b7082f9059263266474c8
Ashutosh Kumar <53196209+TiwariAshuism@users.noreply.github.com>
2022-08-09 22:46:16 +0530

    Day 4 Commit
    I learned about props in react. how props works and how can we call them

--- 100days/src/App.js
+++ 100days/src/App.js
@@ -1,9 +1,17 @@
 import Expenseitem from './Component/Expenseitem'
 
 function App(){
+    let expensedate= new Date(2020, 3 ,19);
+    let expensetitle= 'fee';
+    let expenseamount = 300;
+
     return(
         <div>
-            <Expenseitem />
+            <Expenseitem 
+            date={expensedate}
+            title={expensetitle}
+            amount={expenseamount}
+            />
         </div>
     );
 }
--- 100days/src/Component/Expenseitem.js
+++ 100days/src/Component/Expenseitem.js
@@ -1,12 +1,12 @@
 import "./Expenseitem.css";
 
-function Expenseitem() {
+function Expenseitem(props) {
+    
     return (
-        <div className="topnav">
-            <a class="active" href="#home">Home</a>
-            <a href="#news">News</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
+        <div className="con">
+           
+            <div>{props.date.toISOString}</div>
+        
         </div>
     )
 }
