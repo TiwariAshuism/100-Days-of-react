4cdf8dc0a4f6d6a3ad96eeadc36268cfaa960fee
Ashutosh Kumar <53196209+TiwariAshuism@users.noreply.github.com>
2022-09-07 00:20:30 +0530

    callback

--- Javascripts Concepts/script.js
+++ Javascripts Concepts/script.js
@@ -1,6 +1,30 @@
-function sendmail() {
-    setTimeout(() => { console.log("email sent"); }, 1000);
+function sendmail(callback) {
+    setTimeout(() => { console.log("email sent"), callback(); }, 5000);
 
 }
 
-sendmail();
+function login(callback) {
+    setTimeout(() => { console.log("email login"), callback(); }, 2000);
+}
+
+function getData(callback) {
+    setTimeout(() => {
+        console.log("data fetched"); callback;
+    }, 4000);
+}
+
+function DisplayData() {
+    setTimeout(() => {
+        console.log("Data Displayed");
+    }, 1000);
+}
+
+sendmail(function () {
+    login(function () {
+        getData(function () {
+            DisplayData();
+        });
+
+    });
+
+});
